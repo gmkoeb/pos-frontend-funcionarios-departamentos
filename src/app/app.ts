7061a070f9fa08c51940fadc1ed4e791623b8aa8
gmkoeb <gmkoeb@gmail.com>
2026-03-03 13:28:04 -0300

    add navbar component
    Added navbar component with sandwitch menu button to show/hide menu
    
    Added menu with links to Usuário, Departamentos and funcionários page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DepartamentosPesquisa } from "./departamentos-pesquisa/departamentos-pesquisa";
+import { Navbar } from './navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, DepartamentosPesquisa],
+  imports: [RouterOutlet, DepartamentosPesquisa, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
