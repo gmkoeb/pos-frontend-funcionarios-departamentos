ca2d3475114f84cba9fa18c4227e58c7617282aa
gmkoeb <gmkoeb@gmail.com>
2026-03-03 10:18:02 -0300

    change primeng default theme colors

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,26 @@ import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeuix/themes/aura';
+import Lara from '@primeuix/themes/lara';
+import { definePreset } from '@primeuix/themes';
+
+const laraYellow = definePreset(Lara, {
+    semantic: {
+        primary: {
+            50: '{yellow.50}',
+            100: '{yellow.100}',
+            200: '{yellow.200}',
+            300: '{yellow.300}',
+            400: '{yellow.400}',
+            500: '{yellow.500}',
+            600: '{yellow.600}',
+            700: '{yellow.700}',
+            800: '{yellow.800}',
+            900: '{yellow.900}',
+            950: '{yellow.950}'
+        }
+    }
+});
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -13,7 +32,7 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes), provideClientHydration(withEventReplay()),
     providePrimeNG({
       theme: {
-        preset: Aura
+        preset: laraYellow
       }
     })
   ]
